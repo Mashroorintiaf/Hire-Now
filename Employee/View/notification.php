3f79cf8d20c3e16dfdcf61402084573dd9c53ef8
Mashroorintiaf <ahmedmashroor1921@gmail.com>
2021-07-03 23:44:50 +0600

    test

---
+++ Employee/View/notification.php
@@ -0,0 +1,73 @@
+
+<?php
+
+include('header/header.php');
+
+
+?>
+
+
+
+
+<body>
+
+
+
+<div class="w3-container">
+<center>  <h2>History</h2>
+
+  <table class="w3-table-all w3-centered">
+    <tr>
+     <th>Date & Time</th>
+								<th>Sender Id</th>
+								<th>Reciever Id</th>
+								<th>Massage</th>
+    </tr>
+    <tr>
+      <td>Maria</td>
+     	<tr>
+			<td>6/29/2021</td>
+			<td>Maria123</td>
+			<td>Employee321</td>
+				<td>Make fast i am going outside.</td>
+				            <td style="color:green"><a href = "massagebox.php">Reply</a>
+							  <td style="color:red"><a href = "../Controller/delete.php">Delete</a>
+				
+		</tr>
+    </tr>
+		
+
+  </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+<?php
+
+include('footer/footer.php');
+
+
+
+?>
